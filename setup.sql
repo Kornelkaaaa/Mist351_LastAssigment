908a967bb39bca05740b7732a6a75388fbadfaa7
Kornelia Buszka <110421634+Kornelkaaaa@users.noreply.github.com>
2025-04-04 17:38:50 -0400

    Create setup.sql

---
+++ setup.sql
@@ -0,0 +1,11 @@
+create database Assigment4KB;
+
+use Assigment4KB;
+
+CREATE TABLE DONOR(
+    -> DonorID int not null primary key,
+    -> DonorFN varchar(10),
+    -> DonorLN varchar(10));
+
+CREATE TABLE NUTRITION (
+    -> NutririonID INT NOT NULL PRIMARY KEY);
